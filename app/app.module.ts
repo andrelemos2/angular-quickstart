a599524ab2de378031f98e917a1d09638bf149a0
Andre Lemos <andrelemos2@yahoo.com.br>
2016-11-22 21:27:04 -0200

    Service and Routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,45 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent }   from './app.component';
-
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
 
 @NgModule({
-  imports: [ 
+  imports: [
     BrowserModule,
-    FormsModule
-     ],
-  declarations: [ 
-    AppComponent,
-    HeroDetailComponent
-   ],
-  bootstrap:    [ AppComponent ]
-})
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
 
-export class AppModule { }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
